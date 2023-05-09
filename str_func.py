609cc2e7e99c38b4f65bf8690d1fb1491bab8ac6
Pavel Romanov <denture40@gmail.com>
2023-05-09 17:19:33 +0300

    add_second_function

--- str_func.py
+++ str_func.py
@@ -3,3 +3,7 @@ def  make_up ():
     up_string = small_string.upper()
     return up_string
 
+def make_up_first():
+    """Вторая функция, которая по заданию должна быть первой"""
+    first_up_string = small_string.capitalize()
+    return first_up_string
